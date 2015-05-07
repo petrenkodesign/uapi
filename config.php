4f4fa0a53719fe7d197a3c2101a112f0b26fcad9
Alexey Petrenko <petrenkodesign@gmail.com>
2015-05-07 18:57:27 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,19 @@
 <?php
+//---Universal API ------------
+//---created by Alex Petrenko
+//---http://petrenkodesign.com
+//---petrenkodesign@gmail.com
+//-----------------------------
 // Database config
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'api_db');
 define('DB_USER', 'api_user');
 define('DB_PASS', 'password');
 define('PASSPHRASE', 'were#all#mad#here');
-
+// Included projects scripts
 include('database.php');
 include('security.php');
 include('constructor.php');
 
-$dbfun = new get_api();
+// new get_api(); //<---uncomment for start
 ?>
